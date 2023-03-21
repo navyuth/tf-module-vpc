f28d99635a41d5d8819e77ef2a7273b4b2321100
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-21 23:45:23 +0530

    public route table association

--- main.tf
+++ main.tf
@@ -30,12 +30,12 @@ resource "aws_route_table" "public-route-table" {
   )
 }
 
-## Route table association
-#resource "aws_route_table_association" "public-association" {
-#  for_each = var.public_subnets
-#  subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
-#  route_table_id = aws_route_table[each.value["name"]].id
-#}
+# Route table association {Hard part in terraform referring data}
+resource "aws_route_table_association" "public-association" {
+  for_each = var.public_subnets
+  subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
+  route_table_id = aws_route_table.public-route-table[each.value["name"]].id
+}
 
 # Private Subnets
 resource "aws_subnet" "private_subnets" {
