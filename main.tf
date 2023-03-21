823b8af97d39ad142691991392ff7d2f0ff00961
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-21 23:51:38 +0530

    added lookup function for subnet_id

--- main.tf
+++ main.tf
@@ -33,7 +33,8 @@ resource "aws_route_table" "public-route-table" {
 # Route table association {Hard part in terraform referring data}
 resource "aws_route_table_association" "public-association" {
   for_each = var.public_subnets
-  subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
+  subnet_id = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)
+  #subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
   route_table_id = aws_route_table.public-route-table[each.value["name"]].id
 }
 
