8fd7b511d9c7526c15d31d01dc50958bff43a1f8
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-21 22:27:00 +0530

    resources for public subnets

--- main.tf
+++ main.tf
@@ -8,13 +8,14 @@ resource "aws_vpc" "main" {
 # Public subnets
 resource "aws_subnet" "public_subnets" {
   vpc_id = aws_vpc.main.id
+  cidr_block = each.value["cidr_block"]
   tags = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
   )
 
   for_each = var.public_subnets
-  cidr_block = each.value["cidr_block"]
+
   availability_zone = each.value["availability_zone"]
 
 }
