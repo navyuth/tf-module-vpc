da3dc906d78dd8ed74e4127a73931d26db134037
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-21 23:31:27 +0530

    created public and private route tables

--- main.tf
+++ main.tf
@@ -9,25 +9,48 @@ resource "aws_vpc" "main" {
 # Public subnets
 resource "aws_subnet" "public_subnets" {
   vpc_id = aws_vpc.main.id
-  tags = merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}" }
-  )
 
   for_each = var.public_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+
+# Public route-table
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.public_subnets
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
 }
 
 # Private Subnets
 resource "aws_subnet" "private_subnets" {
   vpc_id = aws_vpc.main.id
-  tags = merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}" }
-  )
+
 
   for_each = var.private_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+
+# Private route table
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.private_subnets
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
 }
