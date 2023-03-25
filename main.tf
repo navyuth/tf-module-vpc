fbfff3c97eccf27abdd6e25e2d53be8f51da563e
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 12:38:20 +0530

    added name tag to peer connection

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept = true
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-peer" }
+  )
 }
 
 # Public subnets
